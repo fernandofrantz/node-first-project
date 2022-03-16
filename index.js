c207b31a8d4028f0e5fdb184bcd8c45b40d9fef7
fernandofrantz <fernandofrantz12@gmail.com>
2022-03-15 21:08:21 -0300

    second commit: url params, instance of express and post

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
-// importando o express e atribuindo a uma constante
+// Importando o express e atribuindo a uma constante
+const express = require("express");
 
-const express = require('express');
-
+// Instanciando um app do express
 const app = express();
 
 /*
-chamando a função do app que irá executar requisições em uma determinada porta
+Chamando a função do app que vai escutar requisições 
+em uma determinada porta
 */
+app.listen(3000, () =>
+  console.log("Aplicação rodando em http://localhost:3000")
+);
 
-// criando uma requisição com GET
-app.get('/', (req, res) => {
-  res.send('Hello World!'); 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
 });
 
-// chamando método listen que indica que o servidor deve escutar, neste caso a porta 3000
-//app.listen(3000, () => console('aplicação rodando em http://localhost:3000'));
+app.post("/", (req, res) => {
+  res.statusCode = 201;
+  res.json({ message: "Resource created!" });
+});
+
+// outro formato da mesma coisa acima
+// app.post("/", (req, res) => {
+//   res.status(201).json({ message: "Resource created!" });
+// });
+
+// adicionando header na resposta da requisiçao
+// app.get("/", (req, res) => {
+//   /*
+//   Adicionando o header que define o tipo de
+//   conteúdo da resposta
+//   */
+//   res.set("Content-Type", "application/json");
+//   res.json({ message: "Testando cabeçalhos" });
+// });
+
+// multiplos headers
+// res.set({
+//   "Content-Type": "text/plain",
+//   "Content-Length": "10000",
+// });
+
+app.get("/resources/:resource_id/subitens/:item_id", (req, res) => {
+  const params = req.params;
+  res.json(params);
+});
+
+// url ex: GET http://localhost:3000/resources/1/subitens/2
+
+app.get("/resources", (req, res) => {
+  const queryParams = req.query;
+  res.json(queryParams);
+});
+
+// url ex: GET http://localhost:3000/resources?order_by=asc&max_itens=100
+
+// INSTANCIANDO APP DO EXPRESS
+// import express from "express";
+// const app = express();
+// app.use(express.json())
+
+// APÓS INSTANCIAR O EXPRESS É POSSÍVEL ACESSAR O BODY DA REQUISIÇÃO DA SEGUINTE FORMA
+// app.post("/resources", (req, res) => {
+//   const data = req.body;
+//   res.json(data);
+// });
